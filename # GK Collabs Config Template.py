9f2b7692f788d0612f861c396a7fe560cbbec8b0
karennelson185 <karen.nelson185@gmail.com>
2026-05-07 08:17:07 +0100

    Delete # GK Collabs Config Template.py
    # deleted file

--- # GK Collabs Config Template.py
+++
@@ -1,9 +0,0 @@
-# GK Collabs Config Template
-# Replace the strings below with your local database credentials
-db_params = {
-    "host": "localhost",
-    "database": "your_database_name",
-    "user": "your_username",
-    "password": "your_password_here",
-    "port": "your_port"
-}
