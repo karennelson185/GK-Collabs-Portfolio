303c23119060237353311296c6ac153e82440162
karennelson185 <karen.nelson185@gmail.com>
2026-05-09 13:34:24 +0100

    Enhance game search with title and genre options
    Updated the game search script to include genre search functionality and improved output formatting.

--- Github - The Games Compendium files/the_games_compendium_search.py
+++ Github - The Games Compendium files/the_games_compendium_search.py
@@ -1,14 +1,45 @@
-# Games Search Script
+# The Games Compendium Search Script
 
 import psycopg2
+from tabulate import tabulate
 
-conn = psycopg2.connect(
-     user="your_username",
+def search_game():
+    print("\n===**   THE GAMES COMPENDIUM:  SEARCH  **===")
+    term = input("\nSearch by Title or Genre: ")
+    try:
+        conn = psycopg2.connect(
+        user="your_username",
         password="your_password", 
         host="your_host", 
         port="your_port",
-        database="your_database"
-)
+        database="your_database
+        )
+        cur = conn.cursor()
+
+        title_query = "SELECT game_id, title, genre FROM games WHERE title ILIKE %s OR genre ILIKE %s ORDER BY game_id ASC"
+        search_data =(f%{term}%", f"%{term}%")
+        cur.execute(title_query, search_data)
+
+        results = cur.fetchall()
+
+        if results
+            print("\n")
+            print("Here are your results: ")
+            print("\n")
+            print(tabulate(results, headers=["ID", "Title", "Genre"], tablefmt="psql"))
+
+        else:
+             print("No games found.  Sorry!")
+        
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+     search_game()
+     
+
 
 print("--- Search for a Game ---")
 search_term = input("Enter game title to search for: ")
